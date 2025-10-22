18108b9d2caf71252f106b698bd0f724f9f72927
Terence Eden <edent@users.noreply.github.com>
2025-10-22 15:25:35 +0100

    Update RSS.ts

--- src/RSS.ts
+++ src/RSS.ts
@@ -4,7 +4,7 @@
  **/
 import { RSSItem } from "./index";
 
-const BASE_URL = "https://azu.github.io/github-search-rss";
+const BASE_URL = "https://edent.github.io/github-search-rss";
 // Query references
 // https://docs.github.com/en/github/searching-for-information-on-github/understanding-the-search-syntax
 // https://docs.github.com/en/github/searching-for-information-on-github/about-searching-on-github
@@ -13,119 +13,17 @@ const BASE_URL = "https://azu.github.io/github-search-rss";
 export const SEARCH_ITEMS: RSSItem[] = [
     // Issue
     {
-        title: "microsoft/vscode Iteration Plan",
-        query: "repo:microsoft/vscode is:issue is:open label:iteration-plan",
+        title: "Blog",
+        query: "shkspr.mobi",
         TYPE: "ISSUE",
-        link: `${BASE_URL}/vscode-iteration-plan.json`,
-        homepage: "https://github.com/microsoft/vscode/issues?q=state%3Aopen+label%3A%22iteration-plan%22"
+        link: `${BASE_URL}/shkspr.json`,
+        homepage: "https://github.com/search?q=%22shkspr.mobi%22"
     },
     {
-        title: "microsoft/TypeScript Iteration Plan",
-        query: "repo:microsoft/TypeScript is:issue label:Planning",
-        TYPE: "ISSUE",
-        link: `${BASE_URL}/typescript-iterator-plan.json`,
-        homepage: "https://github.com/search?q=repo%3Amicrosoft%2FTypeScript+is%3Aissue+label%3APlanning"
-    },
-    {
-        title: "w3ctag/design-reviews Design Issues",
-        query: "repo:w3ctag/design-reviews is:issue",
-        TYPE: "ISSUE",
-        link: `${BASE_URL}/w3ctag-design-reviews.json`
-    },
-    {
-        title: "npm/rfcs Issues",
-        query: "repo:npm/rfcs is:issue",
-        TYPE: "ISSUE",
-        link: `${BASE_URL}/npm-rfcs.json`
-    },
-    {
-        title: "npm/statusboard Issues",
-        query: "repo:npm/statusboard is:issue",
-        TYPE: "ISSUE",
-        link: `${BASE_URL}/npm-statusboard.json`
-    },
-    {
-        title: "github/roadmap Issues",
-        query: "repo:github/roadmap is:issue",
-        TYPE: "ISSUE",
-        link: `${BASE_URL}/github-roadmap.json`
-    },
-    {
-        title: "mozilla/standards-positions Issues",
-        query: "repo:mozilla/standards-positions is:issue",
-        TYPE: "ISSUE",
-        link: `${BASE_URL}/mozilla-standards-positions.json`
-    },
-    {
-        title: "WebKit/standards-positions Issues",
-        query: "repo:WebKit/standards-positions is:issue",
-        TYPE: "ISSUE",
-        link: `${BASE_URL}/WebKit-standards-positions.json`
-    },
-    {
-        title: "Fyrd/caniuse Issues",
-        query: 'repo:Fyrd/caniuse label:"Support data suggestion"',
-        TYPE: "ISSUE",
-        link: `${BASE_URL}/caniuse.json`
-    },
-    {
-        title: "org:wintercg Issues",
-        query: "org:wintercg is:open is:issue -repo:wintercg/admin",
-        TYPE: "ISSUE",
-        link: `${BASE_URL}/wintercg.json`
-    },
-    {
-        title: "repo:babel/proposals Issues",
-        query: "repo:babel/proposals is:issue",
-        TYPE: "ISSUE",
-        link: `${BASE_URL}/babel-proposals.json`
-    },
-    {
-        title: "repo:antifraudcg/proposals Issues",
-        query: "repo:antifraudcg/proposals is:issue",
-        TYPE: "ISSUE",
-        link: `${BASE_URL}/antifraudcg-proposals.json`
-    },
-    // Pull Request
-    {
-        // label:data:
-        title: "mdn/browser-compat-data update data",
-        query: "repo:mdn/browser-compat-data is:pr is:open",
-        TYPE: "ISSUE",
-        link: `${BASE_URL}/mdn-browser-compat-data.json`,
-        filter: (item) => {
-            return item.labels.some((label) => label.startsWith("data:"));
-        }
-    },
-    {
-        title: "mdn/content update content",
-        query: "repo:mdn/content is:pr is:open",
-        TYPE: "ISSUE",
-        link: `${BASE_URL}/mdn-content.json`
-    },
-    {
-        title: "Node.js notable changes",
-        query: "repo:nodejs/node label:notable-change is:pr is:closed -label:doc",
-        TYPE: "ISSUE",
-        link: `${BASE_URL}/nodejs-notable.json`
-    },
-    {
-        title: "whatwg/html changes",
-        query: 'repo:whatwg/html is:pr label:"impacts documentation"',
-        TYPE: "ISSUE",
-        link: `${BASE_URL}/whatwg-html.json`
-    },
-    {
-        title: "papers-we-love new papers",
-        query: "repo:papers-we-love/papers-we-love is:pr is:merged add",
-        TYPE: "ISSUE",
-        link: `${BASE_URL}/papers-we-love.json`
-    },
-    // Repository
-    {
-        title: "LightWeight JavaScript repositories",
-        query: "lightweight language:javascript language:typescript sort:updated-desc",
-        TYPE: "REPOSITORY",
-        link: `${BASE_URL}/lightweight-javascript-repo.json`
+        title: "Blog PR",
+        query: "shkspr.mobi is:pullrequest",
+        TYPE: "PULLREQUEST",
+        link: `${BASE_URL}/shkspr-pr.json`,
+        homepage: "https://github.com/search?q=%22shkspr.mobi%22&type=pullrequests"
     }
 ];
